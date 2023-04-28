ff4237d71f0a4ed420a34e583d4443646d18a42e
Dmytro-Chornomaz <bloody_justice@outlook.com>
2023-04-28 10:18:42 +0200

    Add project files.

---
+++ Hello World/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    public static class Calculator
+    {
+        public static double Addition(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Division(double a, double b)
+        {
+            if(b == 0) { throw new DivideByZeroException("Division by zero!"); }
+            return a / b;
+        }
+
+
+    }
+}
---
+++ Hello World/Class1.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+
+namespace Hello_World
+{
+    public class Class1
+    {
+        [Test]
+        
+        public void AddTest() 
+        {
+            //Arrange
+            double a = 2, b = 3;
+
+            //Act
+            double result = Calculator.Addition(a, b);
+
+            //Assert
+            Assert.AreEqual(a + b, result);
+        }
+
+        [Test]
+
+        public void DivisionTest()
+        {
+            //Arrange
+            double a = 4, b = 4;
+
+            //Act
+            double result = Calculator.Division(a, b);
+
+            //Assert
+            Assert.AreEqual(a / b, result);
+        }
+
+        [Test]
+        public void DivideZeroTest()
+        {
+            //Arrange
+            double a = 0, b = 4;
+
+            //Act
+            double result = Calculator.Division(a, b);
+
+            //Assert
+            Assert.AreEqual(a / b, result);
+        }
+
+        [Test]
+
+        public void DivisionByZeroTest()
+        {
+            try
+            {
+                //Arrange
+                double a = 2, b = 0;
+
+                //Act
+                double result = Calculator.Division(a, b);
+            }
+            catch(DivideByZeroException) 
+            {
+                Assert.Pass("Expected Division by zero caught");
+            }
+            catch(Exception)
+            {
+                Assert.Fail("Wrong exception caught");
+            }
+        }
+    }
+}
