5b865b3692cc9813af97e82e761f26ea485fa103
Dmytro-Chornomaz <bloody_justice@outlook.com>
2023-05-31 19:24:02 +0200

    Added educational materials

---
+++ Amazon/FramesTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AQA_1
+{
+    public class FramesTest
+    {
+        IWebDriver driver;
+
+        [Test]
+        public void NestedFramesTest()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/nestedframes");
+            driver.Manage().Window.Maximize();
+
+            var iframes = driver.FindElements(By.TagName("iframe"));
+            IWebElement parentFrame = driver.FindElement(By.XPath(" //*[@id='frame1']"));
+
+            driver.SwitchTo().Frame(parentFrame);
+
+            IWebElement element = driver.FindElement(By.TagName("body"));
+            Console.WriteLine(element.Text);
+
+            iframes = driver.FindElements(By.TagName("iframe"));
+
+            driver.SwitchTo().Frame(0);
+            var childElement = driver.FindElement(By.TagName("p"));
+            Console.WriteLine(childElement.Text);
+
+            driver.SwitchTo().ParentFrame();
+            element = driver.FindElement(By.TagName("body"));
+            Console.WriteLine(element.Text);
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void FrameTest()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/frames");
+            driver.Manage().Window.Maximize();
+
+            var iframes = driver.FindElements(By.TagName("iframe"));
+            IWebElement bigFrame = driver.FindElement(By.XPath(" //*[@id='frame1']"));
+
+            driver.SwitchTo().Frame(bigFrame);
+
+            IWebElement element = driver.FindElement(By.XPath("//h1[@id='sampleHeading']"));
+
+            driver.SwitchTo().DefaultContent();
+            var outOfFrameElement = driver.FindElement(By.XPath(" //*[@id='framesWrapper']/div"));
+            Console.WriteLine(outOfFrameElement.Text);
+
+            driver.Quit();
+        }
+    }
+}
---
+++ Amazon/NavigationTest.cs
@@ -0,0 +1,155 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AQA_1
+{
+    class NavigationTest
+    {
+        IWebDriver driver;
+        [Test]
+        public void TestNavigation()
+        {
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.amazon.com/");
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Navigate().Back();
+            driver.Navigate().Forward();
+            driver.Navigate().Refresh();
+
+        }
+
+        [Test]
+        public void TestInterrogation()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.amazon.com/");
+
+            string pageSource = driver.PageSource;
+            string title = driver.Title;
+            string url = driver.Url;
+
+            driver.SwitchTo().NewWindow(WindowType.Tab);
+            string currentWindowHandle = driver.CurrentWindowHandle;
+            var allWindowHandles = driver.WindowHandles;
+        }
+
+        [Test]
+        public void TestNavigationBetweenTabsAndCloseOneTab()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/browser-windows");
+            driver.Manage().Window.Maximize();
+
+            var tabButton = driver.FindElement(By.XPath("//button[@id='tabButton']"));
+
+            string parentWindowHandle = driver.CurrentWindowHandle;
+
+            tabButton.Click();
+
+            var allWindowHandles = driver.WindowHandles.ToList();
+
+            string? secondWindow = allWindowHandles.
+                Where(x => x != parentWindowHandle).Select(x => x).FirstOrDefault();
+
+            driver.SwitchTo().Window(secondWindow);
+
+            var sampleHeading = driver.FindElement(By.XPath("//h1"));
+
+            Console.WriteLine(sampleHeading.Text);
+
+            driver.Close();
+
+            driver.SwitchTo().Window(parentWindowHandle);
+
+        }
+
+        [Test]
+        public void TestNavigationBetweenTabsAndCloseAllTabs()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/browser-windows");
+            driver.Manage().Window.Maximize();
+
+            var tabButton = driver.FindElement(By.XPath("//button[@id='tabButton']"));
+
+            string parentWindowHandle = driver.CurrentWindowHandle;
+
+            tabButton.Click();
+
+            var allWindowHandles = driver.WindowHandles.ToList();
+
+            string? secondWindow = allWindowHandles.
+                Where(x => x != parentWindowHandle).Select(x => x).FirstOrDefault();
+
+            driver.SwitchTo().Window(secondWindow);
+
+            var sampleHeading = driver.FindElement(By.XPath("//h1"));
+
+            Console.WriteLine(sampleHeading.Text);
+
+            driver.Quit();
+
+        }
+
+        [Test]
+        public void TestNavigationBetweenWindows()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/browser-windows");
+            driver.Manage().Window.Maximize();
+
+
+            var windowButtonWrapper = driver.FindElement(By.XPath("//button[@id='windowButton']"));
+
+            string parentWindowHandle = driver.CurrentWindowHandle;
+
+            windowButtonWrapper.Click();
+
+            var allWindowHandles = driver.WindowHandles.ToList();
+
+            string? secondWindow = allWindowHandles.
+                Where(x => x != parentWindowHandle).Select(x => x).FirstOrDefault();
+
+            driver.SwitchTo().Window(secondWindow);
+
+            var sampleHeading = driver.FindElement(By.XPath("//h1"));
+
+            Console.WriteLine(sampleHeading.Text);
+
+            driver.SwitchTo().Window(parentWindowHandle);   
+        }
+
+        [Test]
+        public void TestAlerts()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/alerts");
+            driver.Manage().Window.Maximize();
+
+            var alertButton = driver.FindElement(By.Id("alertButton"));
+            var confirmButton = driver.FindElement(By.Id("confirmButton"));
+            var promtButton = driver.FindElement(By.Id("promtButton"));
+
+            alertButton.Click();
+            var alert = driver.SwitchTo().Alert();
+            alert.Accept();
+
+            confirmButton.Click();
+            driver.SwitchTo().Alert().Dismiss();
+
+            promtButton.Click();
+            var text = driver.SwitchTo().Alert().Text;
+            driver.SwitchTo().Alert().SendKeys("Bertold");
+            driver.SwitchTo().Alert().Accept();
+
+            driver.Close();
+        }
+    }
+}
