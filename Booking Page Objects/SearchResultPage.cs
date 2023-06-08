47774281bf1e9ed3ca7061274b78a5b66d2a0d37
Dmytro-Chornomaz <bloody_justice@outlook.com>
2023-06-08 12:59:02 +0200

    Update

--- Booking Page Objects/SearchResultPage.cs
+++ Booking Page Objects/SearchResultPage.cs
@@ -12,7 +12,7 @@ namespace Booking_Page_Objects
     {
         public SearchResultPage(Driver driver) : base(driver) { }
 
-        private List<Element> hotelsList => driver.FindElementsByXpath("//div[@data-testid='property-card']");
+        //private List<Element> hotelsList => driver.FindElementsByXpath("//div[@data-testid='property-card']");
 
         private Element ActualDateStart => driver.FindElementByXpath
             ("//button[@data-testid='date-display-field-start']");
@@ -22,19 +22,17 @@ namespace Booking_Page_Objects
         public string GetActualDateStartText() => ActualDateStart.GetText();
         public string GetActualDateEndText() => ActualDateEnd.GetText();
 
-
         public List<string> GetHotelsAdresses()
         {
             List<string> result = new List<string>();
+            List<Element> hotelsList = driver
+                .FindElementsByXpath("//div[@data-testid='property-card']//span[@data-testid='address']");
 
             foreach (var hotel in hotelsList)
             {
                 //Element hotelAddress = hotel.FindElementByXpath(".//span[@data-testid='address']");
-                //string hotelAddressText = hotelAddress.GetText();
-
-                string hotelAddressText = hotel.GetTextByXpath(".//span[@data-testid='address']");
+                string hotelAddressText = hotel.GetText();
                 result.Add(hotelAddressText);
-
             }
             return result;
         }
